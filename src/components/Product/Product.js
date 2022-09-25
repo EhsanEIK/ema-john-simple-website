e12b1897625e1f95660dd3b111cb4ce9ddcf30c4
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-09-25 12:30:28 +0600

    added cart button on product card

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
-    const { id, name, img,seller,ratings } = props.product;
+    const { id, name, img, seller, ratings } = props.product;
     return (
         <div className='product'>
             <div>
-            <img src={img} alt={img} />
-            <div className='product-info'>
-            <p className='product-title'>{name}</p>
-            <p>Manufacturer: {seller}</p>
-            <p>Ratings: {ratings}</p>
-            </div>
+                <img src={img} alt={img} />
+                <div className='product-info'>
+                    <p className='product-title'>{name}</p>
+                    <p>Manufacturer: {seller}</p>
+                    <p>Ratings: {ratings}</p>
+                </div>
+                <button className='btn-cart'>
+                    <p>Add to Cart</p>
+                </button>
             </div>
         </div>
     );
