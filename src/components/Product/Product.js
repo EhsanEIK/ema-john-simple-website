06936d081048020151fd62efa8878fc465ca928f
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-09-25 11:48:28 +0600

    added products on products-container

---
+++ src/components/Product/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const { id, name, img,seller,ratings } = props.product;
+    return (
+        <div className='product'>
+            <div>
+            <img src={img} alt={img} />
+            <div className='product-info'>
+            <p className='product-title'>{name}</p>
+            <p>Manufacturer: {seller}</p>
+            <p>Ratings: {ratings}</p>
+            </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
     return (
         <div className='shop-container'>
             <div className='products-container'>
-                <h1>products</h1>
+                {
+                    products.map(product => <Product key={product.id} product={product}></Product>)
+                }
             </div>
             <div className='orders-container'>
                 <h1>orders</h1>
