c9c3fad53d804c8397ab990057c0926d61e67a44
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-07 10:10:25 +0600

    set useState for cart in Orders component and sent cart data to Cart component

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
 
 const Orders = () => {
-    const products = useLoaderData();
+    const { initialCart } = useLoaderData();
+    const [cart, setCart] = useState(initialCart);
     return (
-        <div>
-            <h2>This is orders:{products.length}</h2>
+        <div className='shop-container'>
+            <div className='products-container'></div>
+            <div className='cart-container'>
+                <Cart cart={cart}></Cart>
+            </div>
         </div>
     );
 };
--- src/loaders/productsAndCartLoader.js
+++ src/loaders/productsAndCartLoader.js
@@ -2,8 +2,8 @@ import { getStoredCart } from "../utilities/fakedb";
 
 export const productsAndCartLoader = async () => {
     // get products
-    const productData = await fetch('products.json');
-    const products = await productData.json();
+    const productsData = await fetch('products.json');
+    const products = await productsData.json();
 
     // get cart from DB
     const storedCart = getStoredCart();
