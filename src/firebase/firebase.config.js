d1f880cf0f6cac720ce1ccc8781859d3c213d69e
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-17 09:10:39 +0600

    added Login and Signup components with routes and added firebase config

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAxtAkollprzmKrFLKnxKFU0d22QUlGXak",
+    authDomain: "ema-john-simple-50b75.firebaseapp.com",
+    projectId: "ema-john-simple-50b75",
+    storageBucket: "ema-john-simple-50b75.appspot.com",
+    messagingSenderId: "384588019798",
+    appId: "1:384588019798:web:778bf1245898435c5123f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
