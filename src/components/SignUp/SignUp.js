8ea8d5828c2f7b1ae9448a154c1feae5ecf155d3
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-17 09:38:00 +0600

    added design to signup form

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-
-        </div>
+        <form className='form-container'>
+            <h1 className='form-title'>Sign Up</h1>
+            <div className='form-control'>
+                <label htmlFor='name'>Name</label>
+                <input type="text" name="name" id="name" placeholder='name' required />
+            </div>
+            <div className='form-control'>
+                <label htmlFor='email'>Email</label>
+                <input type="email" name="email" id="email" placeholder='email' required />
+            </div>
+            <div className='form-control'>
+                <label htmlFor='password'>Password</label>
+                <input type="password" name="password" id="password" placeholder='password' required />
+            </div>
+            <input className='btn-submit' type="submit" value="Sign Up" />
+            <p className='create-new-account-text'>Already have an account? <Link to='/login' className='create-new-account-link'>Login</Link></p>
+        </form>
     );
 };
 
