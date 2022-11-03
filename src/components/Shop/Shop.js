e01cb4c87a92e916f6d04a135c5187fc52371a17
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-11-03 13:27:38 +0600

    solved the cart issue problems

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -56,15 +56,26 @@ const Shop = () => {
     useEffect(() => {
         const storedCart = getStoredCart();
         const savedCart = [];
-        for (const id in storedCart) {
-            const addedProducts = products.find(product => product._id === id);
-            if (addedProducts) {
-                const quantity = storedCart[id];
-                addedProducts.quantity = quantity;
-                savedCart.push(addedProducts);
-            }
-        }
-        setCart(savedCart);
+        const ids = Object.keys(storedCart);
+        fetch('http://localhost:5000/productByIds', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(ids),
+        })
+            .then(res => res.json())
+            .then(data => {
+                for (const id in storedCart) {
+                    const addedProducts = data.find(product => product._id === id);
+                    if (addedProducts) {
+                        const quantity = storedCart[id];
+                        addedProducts.quantity = quantity;
+                        savedCart.push(addedProducts);
+                    }
+                }
+                setCart(savedCart);
+            })
     }, [products]);
 
     return (
